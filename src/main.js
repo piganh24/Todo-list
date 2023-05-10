26f16100757f34fa1231f7fab59af6820863bc0d
Sash Pigal <sasha.pigal777@gmail.com>
2023-05-10 16:22:28 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("work");
+function add() {
+    var app = document.getElementById('todo-list');
+    var p = document.createElement('div');
+    var taskname = document.getElementById('task-name');
+    var value = taskname === null || taskname === void 0 ? void 0 : taskname.value;
+    p.innerHTML = '<div class="todo-item"><input type="radio" name="task"/><label for="task">' + value + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
+    app === null || app === void 0 ? void 0 : app.appendChild(p);
+    console.log(value);
+    saveLocal();
+}
+function saveLocal() {
+    /*const app = document.getElementById('todo-list');
+    localStorage.setItem("Tasks",app)*/
+}
+var deleteTask = function (e) {
+    e.parentElement.remove();
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log("work");
+function add():void{
+    const app = document.getElementById('todo-list');
+    const p = document.createElement('div');
+    const taskname = document.getElementById('task-name') as HTMLInputElement | null;
+    const value = taskname?.value;
+    p.innerHTML = '<div class="todo-item"><input type="radio" name="task"/><label for="task">' + value + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
+    app?.appendChild(p);
+    console.log(value);
+    saveLocal();
+    
+}
+function saveLocal(): void{
+
+    /*const app = document.getElementById('todo-list');
+    localStorage.setItem("Tasks",app)*/
+}
+
+let deleteTask = (e) => {
+    e.parentElement.remove();
+};
