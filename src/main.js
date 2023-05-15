83a55581c391eae82eaf9770f041dbf328de3ba9
Sash Pigal <sasha.pigal777@gmail.com>
2023-05-15 21:41:27 +0300

    some fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
-function add() {
+/*const addBtn = document.getElementById('add_btn');
+addBtn?.addEventListener("click", createTask);*/
+function createTask() {
+    var div = document.createElement("div");
     var app = document.getElementById('todo-list');
-    var p = document.createElement('div');
-    var taskname = document.getElementById('task-name');
-    var value = taskname === null || taskname === void 0 ? void 0 : taskname.value;
-    p.innerHTML = '<div class="todo-item"><input type="radio" name="task"/><label for="task">' + value + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
-    app === null || app === void 0 ? void 0 : app.appendChild(p);
-    console.log(value);
+    var inputValue = document.getElementById('task-name');
+    var taskName = inputValue === null || inputValue === void 0 ? void 0 : inputValue.value;
+    createHtmlCode(div, taskName);
+}
+function createHtmlCode(div, taskName) {
+    div.innerHTML = '<div class="todo-item" id="added-task"><input type="radio" name="task"/><label for="task">' + taskName + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
+    appendList(div);
+}
+function appendList(div) {
+    var app = document.getElementById('todo-list');
+    app === null || app === void 0 ? void 0 : app.appendChild(div);
     saveLocal();
 }
 function saveLocal() {
+    var taskName = document.getElementById('added-task');
+    var text = taskName === null || taskName === void 0 ? void 0 : taskName.innerHTML;
+    localStorage.setItem("tasks", String(text));
+    var value = localStorage.getItem('tasks');
+}
+function restore() {
     /*const app = document.getElementById('todo-list');
-    localStorage.setItem("Tasks",app)*/
+    const div = document.createElement("div");
+    const text = localStorage.getItem('tasks');
+    div.innerHTML = text;
+    app?.appendChild(String(text));*/
 }
 var deleteTask = function (e) {
     e.parentElement.remove();
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,41 @@
-function add():void{
-    const app = document.getElementById('todo-list');
-    const p = document.createElement('div');
-    const taskname = document.getElementById('task-name') as HTMLInputElement | null;
-    const value = taskname?.value;
-    p.innerHTML = '<div class="todo-item"><input type="radio" name="task"/><label for="task">' + value + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
-    app?.appendChild(p);
-    console.log(value);
-    saveLocal();
-    
-}
-function saveLocal(): void{
+/*const addBtn = document.getElementById('add_btn');
+addBtn?.addEventListener("click", createTask);*/
 
+function createTask(){
+    const div = document.createElement("div");
+    const app = document.getElementById('todo-list');
+    const inputValue = document.getElementById('task-name') as HTMLInputElement | null;
+    const taskName = inputValue?.value;
+    createHtmlCode(div,taskName);
+}
+
+function createHtmlCode(div,taskName){
+    div.innerHTML = '<div class="todo-item" id="added-task"><input type="radio" name="task"/><label for="task">' + taskName + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
+    appendList(div);
+}
+
+function appendList(div):void{
+    const app = document.getElementById('todo-list');
+    app?.appendChild(div);
+    saveLocal();
+}
+
+function saveLocal(){
+    const taskName = document.getElementById('added-task');
+    const text =  taskName?.innerHTML;
+    localStorage.setItem("tasks",String(text));
+    const value = localStorage.getItem('tasks');
+}
+
+function restore(){
     /*const app = document.getElementById('todo-list');
-    localStorage.setItem("Tasks",app)*/
+    const div = document.createElement("div");
+    const text = localStorage.getItem('tasks');
+    div.innerHTML = text;
+    app?.appendChild(String(text));*/
 }
 
 let deleteTask = (e) => {
     e.parentElement.remove();
 };
+
