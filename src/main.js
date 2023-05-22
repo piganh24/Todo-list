1a46ca36ef1ee2f240e7b4aca7d0db5ceb667683
Sash Pigal <sasha.pigal777@gmail.com>
2023-05-22 09:53:05 +0300

    update toDoList

--- src/main.js
+++ src/main.js
@@ -1,34 +1,49 @@
-/*const addBtn = document.getElementById('add_btn');
-addBtn?.addEventListener("click", createTask);*/
-function createTask() {
+document.addEventListener("DOMContentLoaded", function () {
+    var addBtn = document.getElementById("add_btn");
+    addBtn.addEventListener("click", createTask);
+    var restoreBtn = document.getElementById("restore_btn");
+    restoreBtn.addEventListener("click", restoreData);
+    //restore();
+});
+
+const  createTask = () => {
+    var taskName = document.getElementById("task-name").value;
     var div = document.createElement("div");
-    var app = document.getElementById('todo-list');
-    var inputValue = document.getElementById('task-name');
-    var taskName = inputValue === null || inputValue === void 0 ? void 0 : inputValue.value;
     createHtmlCode(div, taskName);
 }
-function createHtmlCode(div, taskName) {
-    div.innerHTML = '<div class="todo-item" id="added-task"><input type="radio" name="task"/><label for="task">' + taskName + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
+
+const createHtmlCode = (div, taskName) => {
+    div.innerHTML = '<div class="todo-item" id="added-task"><input type="radio"/><label for="task">' + taskName + '</label><a onClick="deleteTask(this)"><img src="images/images.png" class="trash-ico" width="15px"/></a></div>';
     appendList(div);
 }
-function appendList(div) {
-    var app = document.getElementById('todo-list');
-    app === null || app === void 0 ? void 0 : app.appendChild(div);
+
+const  appendList = (div) => {
+    var app = document.getElementById("added-tasks");
+    app.appendChild(div);
+    updateLocalStorage();
+}
+
+const  saveLocal = () => {
+    var app = document.getElementById("added-tasks");
+    var text = app.innerHTML;
+    var old = localStorage.getItem("tasks");
+    if (old === null)
+        old = "";
+    localStorage.setItem("tasks", old + text);
+}
+
+const  restoreData = () => {
+    var app = document.getElementById("added-tasks");
+    var localStorageData = localStorage.getItem("tasks");
+    app.innerHTML = localStorageData;
+}
+
+const  deleteTask = (e) => {
+    e.parentElement.remove();
+    updateLocalStorage();
+}
+
+const updateLocalStorage = ()=> {
+    localStorage.clear();
     saveLocal();
 }
-function saveLocal() {
-    var taskName = document.getElementById('added-task');
-    var text = taskName === null || taskName === void 0 ? void 0 : taskName.innerHTML;
-    localStorage.setItem("tasks", String(text));
-    var value = localStorage.getItem('tasks');
-}
-function restore() {
-    /*const app = document.getElementById('todo-list');
-    const div = document.createElement("div");
-    const text = localStorage.getItem('tasks');
-    div.innerHTML = text;
-    app?.appendChild(String(text));*/
-}
-var deleteTask = function (e) {
-    e.parentElement.remove();
-};
